390a4ae5dfb51cf45a226c0f556d47d06be986b1
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-02-02 14:45:06 -0800

    fix: Update minInteger.py solution to include '0' as an integer to include the border condition that the array contains 0 - now works for arrays containing 0!

--- Python/minInteger/minInteger.py
+++ Python/minInteger/minInteger.py
@@ -7,7 +7,7 @@ def solution(A):
     n=1
     i=0
     for i in range(len(A)):
-        if(A[i]>0):
+        if(A[i]>=0):
             # split an array on the current element (one array of all values
             # before, and one array of all values after)
             B = A[0:i-1]
@@ -32,3 +32,5 @@ solution([1, 5, 2, 3]) # 4
 solution([1, 5, 2, 3, 1, 3, 5, 7]) # 4
 solution([7, 6, 5, 4]) # 1
 solution([7, 6, 5, 4, 1, 2]) # 3
+solution([1, 2]) # 3
+solution([-1, 0, 1]) # 2
